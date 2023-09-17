125259e527c3268e56368939db45d59fffec231d
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-17 15:53:13 +0700

    chore: starter project roomdb

--- roomdb/src/main/java/com/ichwan/samples/roomdb/RoomActivity.kt
+++ roomdb/src/main/java/com/ichwan/samples/roomdb/RoomActivity.kt
@@ -1,11 +1,11 @@
-package com.ichwan.store.localstorage
+package com.ichwan.samples.roomdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RoomActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_room)
     }
 }
