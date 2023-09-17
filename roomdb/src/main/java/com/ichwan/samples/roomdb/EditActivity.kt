8e7a68fd4a2d64a997bdb73cb63f44d2a1f0f698
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-17 18:51:54 +0700

    chore: add setup view

--- roomdb/src/main/java/com/ichwan/samples/roomdb/EditActivity.kt
+++ roomdb/src/main/java/com/ichwan/samples/roomdb/EditActivity.kt
@@ -2,6 +2,7 @@ package com.ichwan.samples.roomdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.ichwan.samples.roomdb.databinding.ActivityEditBinding
 import com.ichwan.samples.roomdb.models.Note
 import com.ichwan.samples.roomdb.models.NoteDB
@@ -14,12 +15,16 @@ class EditActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEditBinding
     private lateinit var db: NoteDB
 
+    private var noteId: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         db = NoteDB(this)
+
+        setupView()
 
         binding.apply {
             buttonSave.setOnClickListener {
@@ -28,6 +33,42 @@ class EditActivity : AppCompatActivity() {
                     finish()
                 }
             }
+
+            buttonUpdate.setOnClickListener {
+                CoroutineScope(Dispatchers.IO).launch {
+                    db.noteDao().updateNote(Note(noteId, editTitle.text.toString(), editNote.text.toString()))
+                    finish()
+                }
+            }
+
+        }
+    }
+
+    private fun setupView(){
+        val type = intent.getIntExtra("TYPE",0)
+        when (type) {
+            Constant.TYPE_CREATE -> {
+                binding.buttonSave.visibility = View.GONE
+            }
+            Constant.TYPE_READ -> {
+                binding.buttonSave.visibility = View.GONE
+                binding.buttonUpdate.visibility = View.GONE
+                getNote()
+            }
+            Constant.TYPE_UPDATE -> {
+                binding.buttonSave.visibility = View.GONE
+                getNote()
+            }
+        }
+    }
+
+    private fun getNote(){
+        noteId = intent.getIntExtra("ID",0)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val notes = db.noteDao().getNoteId(noteId)[0]
+            binding.editTitle.setText(notes.title)
+            binding.editNote.setText(notes.note)
         }
     }
 }
