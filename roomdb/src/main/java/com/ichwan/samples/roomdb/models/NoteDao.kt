39a07bc8d8d97c04aa7297d8af7e0615f423bf07
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-17 16:37:59 +0700

    chore: add model and DAO

---
+++ roomdb/src/main/java/com/ichwan/samples/roomdb/models/NoteDao.kt
@@ -0,0 +1,23 @@
+package com.ichwan.samples.roomdb.models
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface NoteDao {
+
+    @Insert
+    suspend fun addNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Query("SELECT * FROM note")
+    suspend fun getNotes(): List<Note>
+}
