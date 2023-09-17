34a88d47e37f81c1d0e602d1d7dbbe827ce844ba
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-18 05:49:21 +0700

    chore: starter project with apollo graphql

---
+++ apollodb/src/main/java/com/ichwan/samples/apollodb/ApolloActivity.kt
@@ -0,0 +1,11 @@
+package com.ichwan.samples.apollodb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ApolloActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apollo)
+    }
+}
